c7f2a22538c816b550eb49b5e513ee379f019bf4
18015566240 <2497533962@qq.com>
2022-07-12 23:29:51 +0800

    1.commit page

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<script setup lang="ts">
+// This starter template is using Vue 3 <script setup> SFCs
+// Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
+import HelloWorld from './components/HelloWorld.vue'
+import HomePage from './components/HomePage.vue'
+</script>
+
+<template>
+  <!-- <img alt="Vue logo" src="./assets/logo.png" />
+  <HelloWorld msg="Hello Vue 3 + TypeScript + Vite" /> -->
+  <HomePage class="mainFrame"></HomePage>
+</template>
+
+<style>
+#app {
+height: 100%;
+}
+
+html,body{
+  height: 100%;
+  margin: unset;
+}
+
+.mainFrame{
+  height: 100%;
+  background-color: #000;
+}
+</style>
---
+++ src/components/HomePage.vue
@@ -0,0 +1,36 @@
+<template>
+    <div class="container">
+        <div style="margin-top:10px">
+            <a style="font-size: 22px;" href="https://wanglantian.github.io/Blog/" target="_blank">Blog</a>
+        </div>
+        <div style="flex:auto;display: flex;flex-direction: column;align-items: center;justify-content: center;">
+            <div style="display: flex;flex-direction: column;align-items: end;">
+                <div style="display: flex;flex-direction: column;align-items: start;">
+                    <h1>“生活不止眼前的苟且，还有诗和远方的田野。</h1>
+                    <h1>你赤手空拳来到人世间，为寻那片海不顾一切。”</h1>
+                </div>
+                <h1>—— 高晓松</h1>
+            </div>
+        </div>
+
+        <div style="margin-bottom:10px">
+            <a href="https://beian.miit.gov.cn/state/outPortal/loginPortal.action">苏ICP备2022024747号</a>
+        </div>
+    </div>
+</template>
+
+<script setup lang="ts">
+import { ref } from 'vue'
+
+</script>
+
+<style scoped>
+.container{
+    height:100%;
+    display:flex;
+    flex-direction: column;
+    
+    align-items: center;
+    color: #fff;
+}
+</style>
